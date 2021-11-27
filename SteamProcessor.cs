a53469a77422176c4ab69510c9876b0bb94a8a08
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-27 14:03:17 +0200

    Implemented login

--- SteamProcessor.cs
+++ SteamProcessor.cs
@@ -1,14 +1,67 @@
 using System;
+using System.Security.Authentication;
 
+using OpenQA.Selenium;
+
+using NuciWeb;
 using NuciWeb.Steam.Models;
 
 namespace NuciWeb.Steam
 {
     public sealed class SteamProcessor : ISteamProcessor
     {
+        public string HomePageUrl => "https://store.steampowered.com";
+        public string LoginUrl => $"{HomePageUrl}/login/?redir=&redir_ssl=1";
+
+        readonly IWebProcessor webProcessor;
+
+        public SteamProcessor(IWebProcessor webProcessor)
+        {
+            this.webProcessor = webProcessor;
+        }
+
         public void LogIn(SteamAccount account)
         {
-            throw new NotImplementedException();
+            webProcessor.GoToUrl(LoginUrl);
+
+            By usernameSelector = By.Id("input_username");
+            By passwordSelector = By.Id("input_password");
+            By captchaInputSelector = By.Id("input_captcha");
+            By logInButtonSelector = By.XPath(@"//*[@id='login_btn_signin']/button");
+            By errorBoxSelector = By.Id("error_display");
+            By steamGuardCodeInputSelector = By.Id("twofactorcode_entry");
+            By avatarSelector = By.XPath(@"//a[contains(@class,'playerAvatar')]");
+
+            if (webProcessor.IsElementVisible(avatarSelector))
+            {
+                throw new AuthenticationException("Already logged in.");
+            }
+            
+            if (webProcessor.IsElementVisible(captchaInputSelector))
+            {
+                throw new AuthenticationException("Captcha input required.");
+            }
+
+            webProcessor.SetText(usernameSelector, account.Username);
+            webProcessor.SetText(passwordSelector, account.Password);
+            
+            webProcessor.Click(logInButtonSelector);
+            webProcessor.WaitForAnyElementToBeVisible(
+                steamGuardCodeInputSelector,
+                errorBoxSelector,
+                avatarSelector);
+
+            if (webProcessor.IsElementVisible(steamGuardCodeInputSelector))
+            {
+                throw new AuthenticationException("Steam Guard input required.");
+            }
+            
+            if (webProcessor.IsElementVisible(errorBoxSelector))
+            {
+                string errorMessage = webProcessor.GetText(errorBoxSelector);
+                throw new AuthenticationException(errorMessage);
+            }
+
         }
     }
 }
