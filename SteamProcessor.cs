132650f82829df674a48ad6e8a32a6d5cb2dfdc9
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-27 19:20:19 +0200

    Improved workshop item favouriting

--- SteamProcessor.cs
+++ SteamProcessor.cs
@@ -134,16 +134,25 @@ namespace NuciWeb.Steam
         {
             GoToWorksopItemPage(workshopItemId);
 
-            By favouriteButtonSelector = By.Id("FavoriteItemOptionAdd");
+            By favouriteButton1Selector = By.Id("FavoriteItemOptionAdd");
+            By favouriteButton2Selector = By.Id("FavoriteItemBtn");
             By unfavouriteButtonSelector = By.Id("FavoriteItemOptionFavorited");
             By favouritedNoticeSelector = By.Id("JustFavorited");
+
+            webProcessor.WaitForAnyElementToBeVisible(
+                favouriteButton1Selector,
+                favouriteButton2Selector,
+                unfavouriteButtonSelector);
 
             if (webProcessor.IsElementVisible(unfavouriteButtonSelector))
             {
                 return;
             }
 
-            webProcessor.Click(favouriteButtonSelector);
+            webProcessor.ClickAny(
+                favouriteButton1Selector,
+                favouriteButton2Selector);
+
             webProcessor.WaitForElementToBeVisible(favouritedNoticeSelector);
         }
 
@@ -153,24 +162,24 @@ namespace NuciWeb.Steam
 
             By subscribeButton1Selector = By.Id("SubscribeItemOptionAdd");
             By subscribeButton2Selector = By.Id("SubscribeItemBtn");
-
             By unsubscribeButtonSelector = By.Id("SubscribeItemOptionSubscribed");
             By subscribedNoticeSelector = By.Id("JustSubscribed");
             By requiredItemSelector = By.ClassName("requiredItem");
+
+            webProcessor.WaitForAnyElementToBeVisible(
+                subscribeButton1Selector,
+                subscribeButton2Selector,
+                unsubscribeButtonSelector);
 
             if (webProcessor.IsElementVisible(unsubscribeButtonSelector))
             {
                 return;
             }
 
-            webProcessor.WaitForAnyElementToBeVisible(
-                subscribeButton1Selector,
-                subscribeButton2Selector);
-
             webProcessor.ClickAny(
                 subscribeButton1Selector,
                 subscribeButton2Selector);
-            
+
             webProcessor.WaitForAnyElementToBeVisible(
                 subscribedNoticeSelector,
                 requiredItemSelector);
