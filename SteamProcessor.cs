722339f4a8526c3b487f8625609c3b945d3de2ae
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-28 14:08:49 +0300

    Logic to set the profile name

--- SteamProcessor.cs
+++ SteamProcessor.cs
@@ -80,6 +80,17 @@ namespace NuciWeb.Steam
             ValidateLogInResult();
         }
 
+        public void SetProfileName(string profileName)
+        {
+            By profileNameSelector = By.Name("personaName");
+            By saveButtonSelector = By.XPath(@"//div[contains(@class,'profileedit_SaveCancelButtons')]/button[@type='submit'][1]");
+
+            GoToEditProfilePage();
+
+            webProcessor.SetText(profileNameSelector, profileName);
+            webProcessor.Click(saveButtonSelector);
+        }
+
         public void AcceptCookies()
         {
             webProcessor.GoToUrl(CookiePreferencesUrl);
@@ -201,6 +212,20 @@ namespace NuciWeb.Steam
             }
 
             webProcessor.WaitForElementToBeVisible(subscribedNoticeSelector);
+        }
+
+        void GoToEditProfilePage()
+        {
+            webProcessor.GoToUrl(AccountUrl);
+
+            By avatarSelector = By.XPath(@"//div[@id='global_actions']/a[contains(@class,'user_avatar')]");
+            By editProfileButton = By.XPath(@"//div[@class='profile_header_actions']/a[contains(@class,'btn_profile_action')][1]");
+
+            webProcessor.WaitForElementToExist(avatarSelector);
+            webProcessor.Click(avatarSelector);
+
+            webProcessor.WaitForElementToExist(editProfileButton);
+            webProcessor.Click(editProfileButton);
         }
 
         void InputSteamGuardCode(string totpKey)
