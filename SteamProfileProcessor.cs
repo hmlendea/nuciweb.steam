2741d9faf25bd60c07f2613c6bf66ed9fb3b3ec4
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2023-11-15 14:46:45 +0200

    profilePicture: Ability to set a profile picture

--- SteamProfileProcessor.cs
+++ SteamProfileProcessor.cs
@@ -27,15 +27,45 @@ namespace NuciWeb.Steam
             webProcessor.Wait(TimeSpan.FromSeconds(1));
         }
 
+        public void SetProfilePicture(string imagePath)
+        {
+            By profilePictureTabSelector = By.XPath(@"//div[contains(@class,'profileeditshell_Navigation')]/a[contains(@href,'/edit/avatar')]");
+            By profilePictureInputSelector = By.XPath(@"//div[contains(@class,'avatar_AvatarDialogUploadArea')]/input");
+            By profilePictureUploadButtonSelector = By.XPath(@"//div[contains(@class,'avatar_AvatarDialogUploadArea')]/button");
+            By profilePictureImageSelector = By.XPath(@"//div[contains(@class,'avatar_AvatarRow')]/div[1]/div[1]/img");
+            By saveButtonSelector = By.XPath(@"//div[contains(@class,'profileedit_SaveCancelButtons')]/button[contains(@class,'Primary')]");
+
+            GoToEditProfilePage();
+
+            webProcessor.Click(profilePictureTabSelector);
+
+            string originalProfilePictureUrl = webProcessor.GetSource(profilePictureImageSelector);
+
+            webProcessor.ExecuteScript("document.evaluate(\"" + profilePictureInputSelector.Criteria +  "\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.style = \"display: block;\";");
+            webProcessor.AppendText(profilePictureInputSelector, imagePath);
+
+            for (int i = 0; i < 5; i++)
+            {
+                string profilePictureUrl = webProcessor.GetSource(profilePictureImageSelector);
+
+                if (profilePictureUrl.Equals(originalProfilePictureUrl))
+                {
+                    webProcessor.Wait();
+                }
+            }
+
+            webProcessor.Click(saveButtonSelector);
+        }
+
         void GoToProfilePage()
         {
             webProcessor.GoToUrl(SteamUrls.Account);
 
             By addFundsLinkSelector = By.XPath(@"//a[@class='account_manage_link'][1]");
-            By avatarSelector = By.XPath(@"//div[@id='global_actions']/a[contains(@class,'user_avatar')]");
+            By profilePictureSelector = By.XPath(@"//div[@id='global_actions']/a[contains(@class,'user_avatar')]");
 
             webProcessor.WaitForElementToExist(addFundsLinkSelector);
-            webProcessor.Click(avatarSelector);
+            webProcessor.Click(profilePictureSelector);
         }
 
         void GoToEditProfilePage()
